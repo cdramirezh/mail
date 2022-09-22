b7c4f530c4b4d9deda525d8a44aed57a0adac0e1
higi <cdramirezh@unal.edu.co>
2022-09-22 16:38:12 -0500

    View email requirement ready

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -24,6 +24,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#individual-email-view').style.display = 'none';
+  
   // Hide weird double mailbox name
   document.querySelector('#weird_double_mailbox_name').style.display = 'none';
   document.querySelector('title').innerHTML = 'Mail'
@@ -39,6 +41,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#individual-email-view').style.display = 'none';
 
   capitalized_mailbox_name = `${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}`
   // Show the mailbox name
@@ -82,12 +85,17 @@ function load_mailbox(mailbox) {
 
         const email_frame = document.createElement('div')
         email_frame.classList.add('row', 'email_frame')
+        // Set a data attribute to access the id of the email
+        // Because when I pass the id directly to the function,
+        // then all the div listers end up using the last emails' id
+        email_frame.setAttribute('data-id', id)
         email_frame.append(sender_frame, subject_frame, timestamp_frame)
 
         // If email is read, it appears with a gray background
         if (read) {email_frame.style.background = 'gray'}
         
         // Add a click handler to the email_frame
+        email_frame.addEventListener('click', function () {click_email(this.dataset.id)})
 
         // Append the <div> to the body
         document.querySelector('#emails-view').append(email_frame)
@@ -123,4 +131,49 @@ function send_email() {
 
   // I need the form not to reload the page
   return false
+}
+
+function click_email(id) {
+  query = `/emails/${id}`
+  fetch(query)
+  .then(response => response.json())
+  .then(email => {
+      
+    // Hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#individual-email-view').style.display = 'block';
+    
+    // Construct individual email frame to show the data
+      // Clean from previous emails
+    document.querySelector('#individual-email-view').innerHTML = ''
+    
+    const sender_frame = document.createElement('p')
+    sender_frame.innerHTML = `<b>From:</b> ${email.sender}`
+    
+    const recipients_frame = document.createElement('p')
+    recipients_frame.innerHTML = `<b>To:</b> ${email.recipients}`
+
+    const subject_frame = document.createElement('p')
+    subject_frame.innerHTML = `<b>Subject:</b> ${email.subject}`
+
+    const timestamp_frame = document.createElement('p')
+    timestamp_frame.innerHTML = `<b>Timestamp:</b> ${email.timestamp}`
+
+    const body_frame = document.createElement('p')
+    body_frame.innerHTML = email.body
+
+    hr = document.createElement('hr')
+    document.querySelector('#individual-email-view').append(sender_frame, recipients_frame, timestamp_frame, hr, body_frame)
+
+  });  
+
+  // Mark the email as read
+  fetch(query, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+  
 }
